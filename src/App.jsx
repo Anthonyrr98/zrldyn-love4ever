350796d3e44854851d9d7006d4d857444ca5c3fc
Anthonyrr <60782320+Anthonyrr98@users.noreply.github.com>
2025-12-02 23:13:02 +0800

    Use HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { AdminPage } from './pages/Admin.jsx';
 import { GalleryPage } from './pages/Gallery.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<GalleryPage />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
